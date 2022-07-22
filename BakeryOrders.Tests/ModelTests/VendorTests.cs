34ee0663a9af9b651f21ab25a8020ddf5acb8b9c
Anthony DiFalco <aodifalco@gmail.com>
2022-07-22 09:13:33 -0700

    pass second test for Vendor class

--- BakeryOrders.Tests/ModelTests/VendorTests.cs
+++ BakeryOrders.Tests/ModelTests/VendorTests.cs
@@ -14,5 +14,20 @@ namespace BakeryOrders.TestClass
       Vendor testVendor = new Vendor("test name", "test description");
       Assert.AreEqual(typeof(Vendor), testVendor.GetType());
     }
+
+    [TestMethod]
+    public void VendorConstructor_ReturnsNameAndDescription_String()
+    {
+      string name = "test name";
+      string description = "test description";
+      Vendor testVendor = new Vendor(name, description);
+      string resultName = testVendor.Name;
+      string resultDescription = testVendor.Description;
+      
+      Assert.AreEqual(name, resultName);
+      Assert.AreEqual(description, resultDescription);
+    }
+
+    
   }
 }
